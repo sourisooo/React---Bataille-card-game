62e0eb2dd6bacad09b1c3d1066f20bd2f540bed3
sourisooo <119761249+sourisooo@users.noreply.github.com>
2024-03-03 11:49:50 +0100

    Update Adversaire.tsx

--- src/components/App/Adversaire.tsx
+++ src/components/App/Adversaire.tsx
@@ -9,9 +9,11 @@ import { useState } from "react";
 
 function Adversairecard() {
 
-    const {deck, player, adversaire} = useSelector((store:any) => store.deck)
+    const {deck, player, adversaire, gameover} = useSelector((store:any) => store.deck)
 
     const [hidden,sethidden] = useState(true);
+
+    const [image, setimage] = useState('url(./favicon.ico)');
 
     const dispatch = useDispatch();
 
@@ -26,8 +28,46 @@ function Adversairecard() {
 
     const toggle = () => {
 
-        if (hidden==true){sethidden(false)} else sethidden(true);
+      if (hidden==true){sethidden(false), setimage('url(./fulldeck.png)'), dispatch({type:'ORDERCARD'});} else {sethidden(true), setimage('url(./favicon.ico)'), dispatch({type:'SHUFFLECARD'});};
 
+    }
+
+    const echangethecard = (event) => {
+
+      checkgamestate();
+
+      if (gameover==false){
+
+      dispatch({type:'ORDERCARD'});
+
+      console.log(event.target.parentNode.id)
+
+      let adversairecard = adversaire.filter(e => e.index ==event.target.parentNode.id);
+
+      console.log(adversairecard);
+
+       dispatch({type:'ECHANGECARDPART1', payload:adversairecard[0]})
+
+       dispatch({type:'ECHANGECARDPART2', payload:player[0]})
+
+       dispatch({type:'SHUFFLECARD'});}
+
+
+    }
+
+    const throwalert = () => {
+
+      alert('you only can trade card while hidden')
+
+
+    }
+
+
+    const checkgamestate = () => {
+  
+      console.log(deck.length);
+     if(deck.length<2){sethidden(true), dispatch({type:'GAMEOVER'}), alert('gameover, please restart the game'), window.location.reload()} 
+  
     }
 
 
@@ -38,12 +78,12 @@ if (hidden==false) {
 
     <button type="button" onClick={toggle}>hide/reveal card</button>
 
-    <div style={{display:'flex', flexDirection:'row'}} onClick={throwthecard}>
+    <div style={{display:'flex', flexDirection:'row'}} onClick={throwalert}>
 
 
     {adversaire.map(card => (
         
-    <Card card={card} />
+    <Card card={card} image={image}/>
 
     ))}
 
@@ -54,7 +94,22 @@ if (hidden==false) {
 
         return(
 
-    <button type="button" onClick={toggle}>hide/reveal adversaire cards</button>
+          <>
+
+          <button type="button" onClick={toggle}>hide/reveal card</button>
+      
+          <div style={{display:'flex', flexDirection:'row'}} onClick={echangethecard}>
+      
+      
+          {adversaire.map(card => (
+              
+          <Card card={card} image={image}/>
+      
+          ))}
+      
+          </div>
+      
+          </>
     )
 
   }
